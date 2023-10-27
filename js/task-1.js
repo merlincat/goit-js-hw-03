512d8712113cd6e5ee9a457a4e3c1b709746f0f8
Nata <40355692+merlincat@users.noreply.github.com>
2023-10-27 23:01:10 +0300

    Add All 3 tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,37 +1,19 @@
-// ----
-// Задача 1. Замовлення дроїдів
-// Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення для відділу продажів.
-// Оголоси функцію makeTransaction(quantity, pricePerDroid, customerCredits), яка складає та повертає повідомлення про купівлю ремонтних дроїдів.
+'use strict';
+// Задача 1. Генератор slug
 
-// Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
 
-// quantity — кількість замовлених дроїдів
-// pricePerDroid — ціна одного дроїда
-// customerCredits — сума коштів на рахунку клієнта
-// Доповни функцію таким чином:
-
-// Оголоси змінну для зберігання загальної суми замовлення (загальна вартість усіх замовлених дроїдів) і задай їй вираз розрахунку цієї суми.
-// Додай перевірку, чи зможе клієнт оплатити замовлення:
-// якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, функція має повертати рядок "Insufficient funds!"
-// в іншому випадку функція має повертати рядок "You ordered <quantity> droids worth <totalPrice> credits!", де <quantity>
-// це кількість замовлених дроїдів, а < totalPrice > це їх загальна вартість.
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalPrice = quantity * pricePerDroid;
-  let errorMessage = 'Insufficient funds!';
-  let orderAccept = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-
-  // if (customerCredits < totalPrice) {
-  //   return errorMessage;
-  // } else {
-  //   return orderAccept;
-  // }
-  return customerCredits < totalPrice ? errorMessage : orderAccept;
+function slugify(title) {
+  let slug = title.toLowerCase().split(' ');
+  return slug.join('-');
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,23 @@
-// Задача 2. Форматування повідомлення
-// Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
+'use strict';
+// Задача 2. Композиція масивів
+// Напиши функцію під назвою makeArray, яка приймає три параметри: firstArray (масив), secondArray (масив) і maxLength (число). Функція повинна створювати новий масив, який містить усі елементи з firstArray, а потім усі елементи з secondArray.
 
-// Доповни код функції таким чином, що:
-
-// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
-// Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
+// Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути копію масиву з довжиною maxLength елементів.
+// В іншому випадку функція повинна повернути весь новий масив.
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
-function formatMessage(message, maxLength) {
-  let messageLength = message.length;
-  let shotrMessage = `${message.slice(0, maxLength)}...`;
-
-  // if (messageLength <= maxLength) {
-  //   return message;
-  // } else {
-  //   return `${shotrMessage}...`;
-  // }
-  return messageLength <= maxLength ? message : shotrMessage;
+function makeArray(firstArray = [], secondArray = [], maxLength = 0) {
+  let thirdArray = firstArray.concat(secondArray);
+  if (thirdArray.length > maxLength) {
+    return thirdArray.slice(0, maxLength);
+  } else {
+    return thirdArray;
+  }
 }
 
-console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
